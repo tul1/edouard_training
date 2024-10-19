7036472dbef14c0789895c19db0ee82af893160e
busin11 <nouyou.edouard@gmail.com>
2024-10-19 12:33:20 +0200

    Reworked copy to fix some mistakes

--- arrays_assignment.go
+++ arrays_assignment.go
@@ -1,18 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+	import (
+	        "fmt"
+	        "math"
+	)
 
-func main() {
-var arr [5]int
-for i:=0;i<len(arr);i++ {
-arr[i] = i*i
-var ode string
-if(arr[i]%2==1){ode="odd"}else{ode="even"}
-fmt.Printf("Element %v : %v , Square : %v , %v", i+1, arr[i], 
-math.Sqrt(float64(arr[i])) ,ode + " \n")
-}
-}
+	func main() {
+
+	var arr [5]int
+
+	for i:=0;i<len(arr);i++ {
+
+	arr[i] = i*i
+
+	var odd string
+
+	if arr[i]%2==1 {
+                odd="odd"
+            } else {
+                odd="even"
+            }
+
+	fmt.Printf("\nElement %#v	: %#v, Square : %#v , %#v", i+1, arr[i], 
+math.Sqrt(float64(arr[i])), odd)
+
+	}}
 
