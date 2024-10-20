98019cb5e015843f85ce0080d99757eaf7bcba1c
busin11 <busin@MacBook-Air-de-busin.local>
2024-10-20 20:29:52 +0200

    Fix arrays_and_loops indentation

--- arrays_and_loops/arrays_and_loop.go
+++ arrays_and_loops/arrays_and_loop.go
@@ -6,15 +6,15 @@ import (
 )
 
 func main() {
-var arr [5]int
-for i:=0;i<len(arr);i++ {
-	arr[i] = i*i
-	var odd string
-	if arr[i]%2==1 {
-		odd="odd"
-	} else {
+	var arr [5]int
+	for i:=0;i<len(arr);i++ {
+		arr[i] = i*i
+		var odd string
+		if arr[i]%2==1 {
+			odd="odd"
+		} else {
 		odd="even"
-	} 
-	fmt.Print("\nElement ", i+1, ": ", arr[i], ",  Square: ", math.Sqrt(float64(arr[i])), ", ",  odd, "\n") 
+		} 
+		fmt.Print("\nElement ", i+1, ": ", arr[i], ",  Square: ", math.Sqrt(float64(arr[i])), ", ",  odd, "\n") 
 	}
 }
