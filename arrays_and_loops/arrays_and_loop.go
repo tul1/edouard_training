b5bf9be635ea3f026e4a00ee8593e506e3db10d9
busin11 <nouyou.edouard@gmail.com>
2024-10-22 18:29:35 +0200

    Add changes to solution (remove + n syntax & odd to pairity)

--- arrays_and_loops/arrays_and_loop.go
+++ arrays_and_loops/arrays_and_loop.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"math"
+        "fmt";
+        "math";
 )
 
+
 func main() {
-	var arr [5]int
-	for i := range arr {
-		arr[i] = i*i
-		var odd string
-		if arr[i]%2==1 {
-			odd="odd"
-		} else {
-			odd="even"
-		} 
-		fmt.Printf("Element %d: %d , Square %#v , %s", i+1, arr[i], math.Sqrt(float64(arr[i])), odd+"\n") 
-	}
+        var arr [5]int
+        var pairity string
+        for i := range arr {
+                arr[i] = i*i
+                if arr[i]%2==1 {
+                        pairity="odd"
+                } else {
+                        pairity="even"
+                }
+                fmt.Printf("Element %d: %d , Square %#v , %s %s", i+1,
+arr[i], math.Sqrt(float64(arr[i])), pairity, "\n")
+        }
 }
 
