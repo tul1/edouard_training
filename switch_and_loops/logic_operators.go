ee9d546c9070104b2b3abc04489be947b0197ca8
busin11 <nouyou.edouard@gmail.com>
2024-11-11 11:42:55 +0100

    Add solution & readme to switch_and_loops

---
+++ switch_and_loops/logic_operators.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+        var user_input int
+        fmt.Print("Enter a number: ")
+        fmt.Scan(&user_input)
+        switch {
+        case user_input>0:
+                fmt.Println("The number is positive")
+                fmt.Printf("Counting down: %d ,", user_input)
+                for user_input!=0 {
+                        user_input-=1
+                        fmt.Print(user_input, ", ")
+                }
+        default:
+                for user_input<0 {
+                        fmt.Print("Please enter a positive number: ")
+                        fmt.Scan(&user_input)
+                }
+                fmt.Printf("You entered a positive number: %d", user_input)
+        case user_input==0:
+                fmt.Print("The number is zero")
+        }
+}
