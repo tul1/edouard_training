1eef0d6e3c891f66943b2fd5826080500ae0bd30
Patricio Tula <tula.patricio@gmail.com>
2024-11-26 00:12:05 +0100

    add lint (#22)
    * add lint
    
    * add files
    
    * add go.mod
    
    * Fix fmt.Scan in strings_and_functions
    
    * Fix fmt.Scan in switch_and_loops
    
    ---------
    
    Co-authored-by: Patricio Tula <patricio.tula@ovhcloud.com>
    Co-authored-by: busin11 <nouyou.edouard@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ed_training
+
+go 1.22.0
--- switch_and_loops/logic_operators.go
+++ switch_and_loops/logic_operators.go
@@ -5,22 +5,36 @@ import "fmt"
 func main() {
         var user_input int
         fmt.Print("Enter a number: ")
-        fmt.Scan(&user_input)
+        _,err := fmt.Scan(&user_input)
+	if err!= nil {
+		fmt.Print(err)
+		return
+	} else {
         switch {
         case user_input>0:
                 fmt.Println("The number is positive")
-                fmt.Printf("Counting down: %d ,", user_input)
+                fmt.Printf("Counting down: %d , ", user_input)
                 for user_input!=0 {
+			if user_input > 1 {
                         user_input-=1
                         fmt.Print(user_input, ", ")
+			} else {
+				user_input-=1
+                       		fmt.Print(user_input)
+			}
                 }
         default:
 		fmt.Print("The number is zero")
 	case user_input<0:		
                 for user_input<0 {
                         fmt.Print("Please enter a positive number: ")
-                        fmt.Scan(&user_input)
+                        _,err := fmt.Scan(&user_input)
+			        if err!= nil {
+					fmt.Print(err)
+					return
+				}
                 }
                 fmt.Printf("You entered a positive number: %d", user_input)
         }
+	}
 }
